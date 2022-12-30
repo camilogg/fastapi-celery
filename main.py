8d1bd926a12687cc2b15672237858f22bdd66331
Adrian <adriancamilo.gg@gmail.com>
2022-12-30 04:01:16 -0500

    Add fastapi project

---
+++ main.py
@@ -0,0 +1,13 @@
+from celery import Celery
+from project import create_app
+
+app = create_app()
+
+
+celery = Celery(
+    __name__,
+    broker="redis://127.0.0.1:6379/0",
+    backend="redis://127.0.0.1:6379/0"
+)
+
+
